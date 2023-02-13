6a09a25fc5ecc96413f87dbcd19fcb55639702fd
Kee <shikee.maloney@codeimmersives.com>
2023-02-13 14:32:30 -0500

    all

--- routes/blogs.js
+++ routes/blogs.js
@@ -24,5 +24,85 @@ router.get('/', async function(req, res, next) {
     });
 
     
-}); 
+
+});
+
+/* GET users listing. */
+
+
+
+router.get('/all', async function(req, res, next) {
+
+  const blogsAll = await db()
+  .collection('sample_blogs')
+  .find({})
+  .limit(5)
+  .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send('Error fetching listings!');
+      } else {
+        res.json(result);
+      }
+    });
+
+    res.json({
+      sucess: true,
+      blogs: blogsAll
+    });
+
+});
+
+router.get('/get-one', async function (req, res, next) {
+  
+
+  const title = req.query.title
+ 
+  const queryFind = await db()
+  .collection('sample_blogs')
+  .find({title: title})
+  .limit(5)
+  .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send('Error fetching listings!');
+      } else {
+        res.json(result);
+      }
+    });
+
+    res.json({
+      sucess: true,
+      blogs: queryFind
+    });
+});
+
+    
+   
+
+
+
+router.get('/single-blog/:titleToGet', async function (req, res, next) {
+    
+ const titleToGet = req.params.titleToGet
+    
+  const queryFind = await db()
+  .collection('sample_blogs')
+  .find({title: titleToGet})
+  .limit(5)
+  .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send('Error fetching listings!');
+      } else {
+        res.json(result);
+      }
+    });
+
+    res.json({
+      sucess: true,
+      blogs: queryFind
+    });
+          
+ })
+
+
+
 module.exports = router;
